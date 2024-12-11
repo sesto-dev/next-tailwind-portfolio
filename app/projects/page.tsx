27041f0e528c943e402dfcaa6582286f84512bcb
Amirhossein Mohammadi <accretence@outlook.com>
2024-12-11 20:43:36 +0330

    new site

--- app/projects/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CardGrid } from "./_components/card-grid";
-
-export default function Projects() {
-  return (
-    <div className="max-w-5xl mx-auto">
-      <CardGrid />
-    </div>
-  );
-}
---
+++ src/data/resume.tsx
@@ -0,0 +1,283 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, NotebookIcon } from "lucide-react";
+
+export const DATA = {
+  name: "Amirhossein Mohammadi",
+  initials: "AM",
+  url: "https://sesto.dev",
+  location: "Tehran, Iran",
+  locationLink: "https://www.google.com/maps/place/tehran",
+  description:
+    "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on YouTube.",
+  summary:
+    "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
+  avatarUrl: "/me.jpg",
+  skills: [
+    "React",
+    "Next.js",
+    "Typescript",
+    "Node.js",
+    "Python",
+    "Postgres",
+    "Docker",
+    "Kubernetes",
+  ],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+    { href: "/blog", icon: NotebookIcon, label: "Blog" },
+  ],
+  contact: {
+    email: "sesto@post.com",
+    tel: "+989122396706",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://github.com/sesto-dev",
+        icon: Icons.github,
+
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/sesto-dev",
+        icon: Icons.linkedin,
+
+        navbar: true,
+      },
+      X: {
+        name: "X",
+        url: "https://x.com/sesto_dev",
+        icon: Icons.x,
+
+        navbar: false,
+      },
+      Telegram: {
+        name: "Telegram",
+        url: "https://t.me/sesto_dev",
+        icon: Icons.telegram,
+        navbar: true,
+      },
+      Youtube: {
+        name: "Youtube",
+        url: "https://www.youtube.com/@sesto-dev",
+        icon: Icons.youtube,
+        navbar: true,
+      },
+      email: {
+        name: "Send Email",
+        url: "#",
+        icon: Icons.email,
+
+        navbar: false,
+      },
+    },
+  },
+  work: [
+    {
+      company: "Boot",
+      href: "https://boot.ir/",
+      badges: [],
+      location: "Tehran, IR",
+      title: "Software Engineer",
+      logoUrl: "/boot.png",
+      start: "January 2024",
+      end: "Present",
+      description:
+        "Implemented the Bitcoin discreet log contract (DLC) protocol specifications as an open source Typescript SDK. Dockerized all microservices and setup production kubernetes cluster. Architected a data lake using AWS S3 and Athena for historical backtesting of bitcoin trading strategies. Built a mobile app using react native and typescript.",
+    },
+    {
+      company: "Henria",
+      badges: [],
+      href: "https://henria.ir/",
+      location: "Tehran, IR",
+      title: "Software Engineer",
+      logoUrl: "/henria.png",
+      start: "January 2021",
+      end: "April 2021",
+      description:
+        "Implemented a custom Kubernetes controller in Go to automate the deployment of MySQL and ProxySQL custom resources in order to enable 2,000+ internal developers to instantly deploy their app databases to production. Wrote several scripts in Go to automate MySQL database failovers while maintaining master-slave replication topologies and keeping Zookeeper nodes consistent with changes.",
+    },
+    {
+      company: "MCI R&D Center",
+      href: "https://mci.ir/",
+      badges: [],
+      location: "Tehran, IR",
+      title: "Software Engineer",
+      logoUrl: "/mci.png",
+      start: "January 2020",
+      end: "April 2020",
+      description:
+        "Architected and wrote the entire MVP of the GeForce Now Cloud Gaming internal admin and A/B testing dashboard using React, Redux, TypeScript, and Python.",
+    },
+    {
+      company: "Saman Insurance",
+      href: "https://www.si24.ir/",
+      badges: [],
+      location: "Tehran, IR",
+      title: "Software Engineer",
+      logoUrl: "/saman.png",
+      start: "January 2019",
+      end: "April 2019",
+      description:
+        "Co-developed a prototype iOS app with another intern in Swift for the new Splunk Phantom security orchestration product (later publicly demoed and launched at .conf annual conference in Las Vegas). Implemented a realtime service for the iOS app in Django (Python) and C++; serialized data using protobufs transmitted over gRPC resulting in an approximate 500% increase in data throughput.",
+    },
+    {
+      company: "Bytogene",
+      href: "https://www.linkedin.com/company/bytogene/people/",
+      badges: [],
+      location: "Tehran, IR",
+      title: "Software Engineer",
+      logoUrl: "/bytogene.jpeg",
+      start: "January 2018",
+      end: "April 2018",
+      description:
+        "Proposed and implemented an internal ruby API for sending/receiving commands to scooters over LTE networks. Developed a fully automated bike firmware update system to handle asynchronous firmware updates of over 100,000+ scooters worldwide, and provide progress reports in real-time using React, Ruby on Rails, PostgreSQL and AWS EC2 saving hundreds of developer hours.",
+    },
+    {
+      company: "Farazin",
+      href: "https://farazin.co.ir/",
+      badges: [],
+      location: "Tehran, IR",
+      title: "Software Engineer Intern",
+      logoUrl: "/farazin.png",
+      start: "May 2017",
+      end: "August 2017",
+      description:
+        "Designed and implemented a robust password encryption and browser cookie storage system in Ruby on Rails. Leveraged the Yahoo finance API to develop the dividend.com equity screener",
+    },
+  ],
+  education: [
+    {
+      school: "Pars University",
+      href: "https://pu.ac.ir/",
+      degree: "MSc. Architectural Technology",
+      logoUrl: "/pars.jpg",
+      start: "2023",
+      end: "2024",
+    },
+  ],
+  projects: [
+    {
+      title: "Chat Collect",
+      href: "https://chatcollect.com",
+      dates: "Jan 2024 - Feb 2024",
+      active: true,
+      description:
+        "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Stripe",
+        "Shadcn UI",
+        "Magic UI",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://chatcollect.com",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "",
+      video:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
+    },
+    {
+      title: "Magic UI",
+      href: "https://magicui.design",
+      dates: "June 2023 - Present",
+      active: true,
+      description:
+        "Designed, developed and sold animated UI components for developers.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Stripe",
+        "Shadcn UI",
+        "Magic UI",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://magicui.design",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/magicuidesign/magicui",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "https://cdn.magicui.design/bento-grid.mp4",
+    },
+    {
+      title: "llm.report",
+      href: "https://llm.report",
+      dates: "April 2023 - September 2023",
+      active: true,
+      description:
+        "Developed an open-source logging and analytics platform for OpenAI: Log your ChatGPT API requests, analyze costs, and improve your prompts.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Shadcn UI",
+        "Magic UI",
+        "Stripe",
+        "Cloudflare Workers",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://llm.report",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/dillionverma/llm.report",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "https://cdn.llm.report/openai-demo.mp4",
+    },
+    {
+      title: "Automatic Chat",
+      href: "https://automatic.chat",
+      dates: "April 2023 - March 2024",
+      active: true,
+      description:
+        "Developed an AI Customer Support Chatbot which automatically responds to customer support tickets using the latest GPT models.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Shadcn UI",
+        "Magic UI",
+        "Stripe",
+        "Cloudflare Workers",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://automatic.chat",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "",
+      video:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/automatic-chat.mp4",
+    },
+  ],
+} as const;
